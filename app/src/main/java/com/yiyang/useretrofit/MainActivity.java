56f4c87e6628d6dc4557c96c9a54e70b49090d45
user <user@gmail.com>
2016-11-04 14:21:04 +0800

    Retrofit work!

--- app/src/main/java/com/yiyang/useretrofit/MainActivity.java
+++ app/src/main/java/com/yiyang/useretrofit/MainActivity.java
@@ -2,6 +2,17 @@ package com.yiyang.useretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +22,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+
+//        Call<List<Repo>> repos = service.listRepos("octocat");
+        Call<List<Repo>> repos = service.listRepos("chiangyiyang");
+
+        //非同步呼叫
+        repos.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                List<Repo> result = response.body();
+                for (item:
+                     result) {
+                    Log.d("Github Data", item.name);
+                }
+
+                Log.d("Yiyang", "DDD");
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
+
+}
+
+interface GitHubService {
+    @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos(@Path("user") String user);
+}
+
+class Repo {
+    int id;
+    String name;
 }
