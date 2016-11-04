32f775e30e2d25040d1441005283614a62a290a4
user <user@gmail.com>
2016-11-04 16:25:36 +0800

    Retrofit Test PHP

--- app/src/main/java/com/yiyang/useretrofit/MainActivity.java
+++ app/src/main/java/com/yiyang/useretrofit/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Iterator;
 import java.util.List;
 
 import retrofit2.Call;
@@ -13,6 +14,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,45 +24,101 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+//
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("https://api.github.com/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//
+//        GitHubService service = retrofit.create(GitHubService.class);
+//
+//        Call<List<Repo>> repos = service.listRepos("octocat");
+////        Call<List<Repo>> repos = service.listRepos("chiangyiyang");
+//
+//        //非同步呼叫
+//        repos.enqueue(new Callback<List<Repo>>() {
+//            @Override
+//            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+//                List<Repo> result = response.body();
+//                for (Repo item:
+//                     result) {
+//                    Log.d("Github Data", "ID: " + item.id + "  Name: " + item.name + " Full Name: " + item.full_name);
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<Repo>> call, Throwable t) {
+//                t.printStackTrace();
+//            }
+//        });
+//    }
 
+
+        //測試PHP網頁
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
+                .baseUrl("http://192.168.58.18:8081/")      //for emulator
+//                .baseUrl("http://192.168.43.233:8081/")   //for real device
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        GitHubService service = retrofit.create(GitHubService.class);
-
-//        Call<List<Repo>> repos = service.listRepos("octocat");
-        Call<List<Repo>> repos = service.listRepos("chiangyiyang");
+        ClassDBService service = retrofit.create(ClassDBService.class);
+//        final Call<List<Student>> repos = service.getStudentData("1");
+        final Call<List<Student>> repos = service.getAllStudentData();
 
         //非同步呼叫
-        repos.enqueue(new Callback<List<Repo>>() {
+        repos.enqueue(new Callback<List<Student>>() {
             @Override
-            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
-                List<Repo> result = response.body();
-                for (item:
-                     result) {
-                    Log.d("Github Data", item.name);
+            public void onResponse(Call<List<Student>> call, Response<List<Student>> response) {
+                List<Student> result = response.body();
+                Iterator it = result.iterator();
+
+                while (it.hasNext()) {
+                    Student student = (Student) it.next();
+                    System.out.println(
+                            "ID: " + student.cID
+                                    + "  Name: " + student.cName
+                                    + " Email: " + student.cEmail
+                    );
                 }
 
-                Log.d("Yiyang", "DDD");
             }
 
             @Override
-            public void onFailure(Call<List<Repo>> call, Throwable t) {
+            public void onFailure(Call<List<Student>> call, Throwable t) {
                 t.printStackTrace();
             }
         });
     }
-
 }
 
-interface GitHubService {
-    @GET("users/{user}/repos")
-    Call<List<Repo>> listRepos(@Path("user") String user);
+
+//
+//interface GitHubService {
+//    @GET("users/{user}/repos")
+//    Call<List<Repo>> listRepos(@Path("user") String user);
+//}
+//
+//class Repo {
+//    int id;
+//    String name;
+//    String full_name;
+//}
+
+
+//http://localhost:8081/code/11-14_project/api/api_read_get.php?cID=1
+//interface ClassDBService {
+//    @GET("code/11-14_project/api/api_read_get.php")
+//    Call<List<Student>> getStudentData(@Query("cID") String cID);
+//}
+
+interface ClassDBService {
+    @GET("code/11-14_project/api/api_read_all_get.php")
+    Call<List<Student>> getAllStudentData();
 }
 
-class Repo {
-    int id;
-    String name;
+class Student {
+    int cID;
+    String cName;
+    String cEmail;
 }
