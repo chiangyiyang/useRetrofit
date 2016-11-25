3e124aaf0cc6dfc4473bef75dc4698b8ce904428
user <user@gmail.com>
2016-11-25 15:28:02 +0800

    修改 1.新增版面配置 1.刪除版面配置 1.修改版面配置

--- app/src/main/java/com/yiyang/useretrofit/ClassDBService.java
+++ app/src/main/java/com/yiyang/useretrofit/ClassDBService.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 
@@ -12,27 +15,53 @@ public interface ClassDBService {
     @GET("code/11-14_project/api/api_read_all_get.php")
     Call<List<Student>> getAllStudentData();
 
-    @GET("code/11-14_project/api/api_del_get.php")
-    Call<ResponseBody> delStudentData(@Query("cID") String cID);
+//    @GET("code/11-14_project/api/api_del_get.php")
+//    Call<ResponseBody> delStudentData(@Query("cID") String cID);
+    @FormUrlEncoded
+    @POST("code/11-14_project/api/api_del_post.php")
+    Call<ResponseBody> delStudentData(@Field("cID") String cID);
 
-    @GET("code/11-14_project/api/api_add_get.php")
+//    @GET("code/11-14_project/api/api_add_get.php")
+//    Call<ResponseBody> addStudentData(
+//            @Query("cName") String cName,
+//            @Query("cSex") String cSex,
+//            @Query("cBirthday") String cBirthday,
+//            @Query("cEmail") String cEmail,
+//            @Query("cPhone") String cPhone,
+//            @Query("cAddr") String cAddr
+//    );
+
+    @FormUrlEncoded
+    @POST("code/11-14_project/api/api_add_post.php")
     Call<ResponseBody> addStudentData(
-            @Query("cName") String cName,
-            @Query("cSex") String cSex,
-            @Query("cBirthday") String cBirthday,
-            @Query("cEmail") String cEmail,
-            @Query("cPhone") String cPhone,
-            @Query("cAddr") String cAddr
+            @Field("cName") String cName,
+            @Field("cSex") String cSex,
+            @Field("cBirthday") String cBirthday,
+            @Field("cEmail") String cEmail,
+            @Field("cPhone") String cPhone,
+            @Field("cAddr") String cAddr
     );
 
-    @GET("code/11-14_project/api/api_update_get.php")
+//    @GET("code/11-14_project/api/api_update_get.php")
+//    Call<ResponseBody> modifyStudentData(
+//            @Query("cID") String cID,
+//            @Query("cName") String cName,
+//            @Query("cSex") String cSex,
+//            @Query("cBirthday") String cBirthday,
+//            @Query("cEmail") String cEmail,
+//            @Query("cPhone") String cPhone,
+//            @Query("cAddr") String cAddr
+//    );
+
+    @FormUrlEncoded
+    @POST("code/11-14_project/api/api_update_post.php")
     Call<ResponseBody> modifyStudentData(
-            @Query("cID") String cID,
-            @Query("cName") String cName,
-            @Query("cSex") String cSex,
-            @Query("cBirthday") String cBirthday,
-            @Query("cEmail") String cEmail,
-            @Query("cPhone") String cPhone,
-            @Query("cAddr") String cAddr
+            @Field("cID") String cID,
+            @Field("cName") String cName,
+            @Field("cSex") String cSex,
+            @Field("cBirthday") String cBirthday,
+            @Field("cEmail") String cEmail,
+            @Field("cPhone") String cPhone,
+            @Field("cAddr") String cAddr
     );
 }
