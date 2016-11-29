18abc848275b209964a3b950ed93abec0c92eba3
user <user@gmail.com>
2016-11-29 11:07:13 +0800

    修改ListView Layout，增加"修改"及"刪除"按鈕，選單"點擊"及"長按"事件

--- app/src/main/java/com/yiyang/useretrofit/MainActivity.java
+++ app/src/main/java/com/yiyang/useretrofit/MainActivity.java
@@ -1,13 +1,19 @@
 package com.yiyang.useretrofit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -25,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_DEL = 0;
     private static final int REQUEST_ADD = 1;
     private static final int REQUEST_MODIFY = 2;
+
+
+
+    private ListView student_list;
+    private MyAdapter myAdapter;
+
+    //    private ArrayAdapter<String> dataAdapter;
+    private List<Student> studentData;
 
 
     @Override
@@ -91,15 +105,20 @@ public class MainActivity extends AppCompatActivity {
         updateList();
     }
 
-
-    private ListView student_list;
-    private ArrayAdapter<String> dataAdapter;
-    private List<Student> studentData;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        student_list = (ListView) findViewById(R.id.lstData);
+
+        studentData = new ArrayList<Student>();
+//        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+//        student_list.setAdapter(dataAdapter);
+
+        myAdapter = new MyAdapter(this);
+        student_list.setAdapter(myAdapter);
+
 
         //Update button
         Button btnUpdate = (Button) findViewById(R.id.btnUpdate);
@@ -130,55 +149,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        studentData = new ArrayList<Student>();
-
-        dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        student_list = (ListView) findViewById(R.id.lstData);
-        student_list.setAdapter(dataAdapter);
-
-        AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Toast.makeText(MainActivity.this, dataAdapter.getItem(i), Toast.LENGTH_LONG).show();
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, ModifyActivity.class);
-
-                intent.putExtra("id", String.valueOf(studentData.get(i).cID));
-                intent.putExtra("name", studentData.get(i).cName);
-                intent.putExtra("gender", studentData.get(i).cSex);
-                intent.putExtra("birthday", studentData.get(i).cBirthday);
-                intent.putExtra("email", studentData.get(i).cEmail);
-                intent.putExtra("tel", studentData.get(i).cPhone);
-                intent.putExtra("addr", studentData.get(i).cAddr);
-
-                startActivityForResult(intent, REQUEST_MODIFY);
-            }
-        };
-
-        AdapterView.OnItemLongClickListener onItemLongClickListener = new AdapterView.OnItemLongClickListener() {
-
-
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, DeleteActivity.class);
-
-                intent.putExtra("id", studentData.get(i).cID);
-                intent.putExtra("name", studentData.get(i).cName);
-                intent.putExtra("gender", studentData.get(i).cSex);
-                intent.putExtra("birthday", studentData.get(i).cBirthday);
-                intent.putExtra("email", studentData.get(i).cEmail);
-                intent.putExtra("tel", studentData.get(i).cPhone);
-                intent.putExtra("addr", studentData.get(i).cAddr);
-
-                startActivityForResult(intent, REQUEST_DEL);
-                return false;
-            }
-        };
-
-        student_list.setOnItemClickListener(onItemClickListener);
-        student_list.setOnItemLongClickListener(onItemLongClickListener);
+//        AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+////                Toast.makeText(MainActivity.this, dataAdapter.getItem(i), Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, ModifyActivity.class);
+//
+//                intent.putExtra("id", String.valueOf(studentData.get(i).cID));
+//                intent.putExtra("name", studentData.get(i).cName);
+//                intent.putExtra("gender", studentData.get(i).cSex);
+//                intent.putExtra("birthday", studentData.get(i).cBirthday);
+//                intent.putExtra("email", studentData.get(i).cEmail);
+//                intent.putExtra("tel", studentData.get(i).cPhone);
+//                intent.putExtra("addr", studentData.get(i).cAddr);
+//
+//                startActivityForResult(intent, REQUEST_MODIFY);
+//            }
+//        };
+//
+//        AdapterView.OnItemLongClickListener onItemLongClickListener = new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, DeleteActivity.class);
+//
+//                intent.putExtra("id", studentData.get(i).cID);
+//                intent.putExtra("name", studentData.get(i).cName);
+//                intent.putExtra("gender", studentData.get(i).cSex);
+//                intent.putExtra("birthday", studentData.get(i).cBirthday);
+//                intent.putExtra("email", studentData.get(i).cEmail);
+//                intent.putExtra("tel", studentData.get(i).cPhone);
+//                intent.putExtra("addr", studentData.get(i).cAddr);
+//
+//                startActivityForResult(intent, REQUEST_DEL);
+//                return false;
+//            }
+//        };
+//
+//        student_list.setOnItemClickListener(onItemClickListener);
+//        student_list.setOnItemLongClickListener(onItemLongClickListener);
 
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -194,10 +204,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateList() {
-        final Call<List<Student>> repos = ((MyApp) getApplicationContext()).api_service.getAllStudentData();
+        final Call<List<Student>> request = ((MyApp) getApplicationContext()).api_service.getAllStudentData();
 
         //非同步呼叫
-        repos.enqueue(new Callback<List<Student>>() {
+        request.enqueue(new Callback<List<Student>>() {
             @Override
             public void onResponse(Call<List<Student>> call, Response<List<Student>> response) {
 //                List<Student> result = response.body();
@@ -207,14 +217,15 @@ public class MainActivity extends AppCompatActivity {
                 app.allStudentData = response.body();
                 Iterator it = app.allStudentData.iterator();
 
-                dataAdapter.clear();
+//                dataAdapter.clear();
                 studentData.clear();
 
                 while (it.hasNext()) {
                     Student student = (Student) it.next();
-                    dataAdapter.add(student.cName);
+//                    dataAdapter.add(student.cName);
                     studentData.add(student);
                 }
+                myAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -222,5 +233,82 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+
+    }
+
+
+    private class MyAdapter extends BaseAdapter{
+        private final LayoutInflater myInflater;
+
+        public MyAdapter(Context context){
+            myInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+
+        public int getCount() {
+            return studentData.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return studentData.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(final int i, View view, ViewGroup viewGroup) {
+
+            View curView = myInflater.inflate(R.layout.my_list_view_layout,null);
+
+            final TextView txtInfo = (TextView) curView.findViewById(R.id.txtInfo);
+            txtInfo.setText(studentData.get(i).cName);
+
+            Button btnDel = (Button) curView.findViewById(R.id.btnDel);
+            btnDel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                    Toast.makeText(MainActivity.this, "DEL:" + txtInfo.getText().toString(),Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, DeleteActivity.class);
+
+                    intent.putExtra("id", studentData.get(i).cID);
+                    intent.putExtra("name", studentData.get(i).cName);
+                    intent.putExtra("gender", studentData.get(i).cSex);
+                    intent.putExtra("birthday", studentData.get(i).cBirthday);
+                    intent.putExtra("email", studentData.get(i).cEmail);
+                    intent.putExtra("tel", studentData.get(i).cPhone);
+                    intent.putExtra("addr", studentData.get(i).cAddr);
+
+                    startActivityForResult(intent, REQUEST_DEL);
+                }
+            });
+
+            Button btnModify = (Button) curView.findViewById(R.id.btnModify);
+            btnModify.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+//                    Toast.makeText(MainActivity.this, "MODIFY:" + txtInfo.getText().toString(),Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent();
+                    intent.setClass(MainActivity.this, ModifyActivity.class);
+
+                    intent.putExtra("id", String.valueOf(studentData.get(i).cID));
+                    intent.putExtra("name", studentData.get(i).cName);
+                    intent.putExtra("gender", studentData.get(i).cSex);
+                    intent.putExtra("birthday", studentData.get(i).cBirthday);
+                    intent.putExtra("email", studentData.get(i).cEmail);
+                    intent.putExtra("tel", studentData.get(i).cPhone);
+                    intent.putExtra("addr", studentData.get(i).cAddr);
+
+                    startActivityForResult(intent, REQUEST_MODIFY);
+                }
+            });
+
+            return curView;
+        }
     }
 }
